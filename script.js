ad13101f40ef355f5e7acc1a7a467f3effd9f7e1
Alex <rubanovv.alex@gmail.com>
2023-01-01 01:17:07 +0100

    added break session + break animation

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 'use strict';
 
+const SESSION_TIME_COLOR = 'rgb(250, 128, 114)';
+const BREAK_TIME_COLOR = 'rgb(107,142,35)';
+const SVG_STROKE_DASHOFFSET = -943;
+
 const minutes = document.querySelector('.timer-minutes');
 const seconds = document.querySelector('.timer-seconds');
 const playBtn = document.querySelector('.play-btn');
 const resetBtn = document.querySelector('.reset-btn');
+const circleBorder = document.querySelector('.app-circle svg circle');
 const circleTimer = document.querySelector('.app-circle svg circle:nth-child(2)');
 
 const totalTime = 25 * 60;
 const breakTime = 5 * 60;
+
 let remainingSessionTime = totalTime;
-let remainingBreakTime = breakTime;
 let intervalID;
+let breakID = true;
 
 // Step for circleTimeProgress and updateCircleTime()
-const step = +(-943 / totalTime); 
+let step = +(SVG_STROKE_DASHOFFSET / totalTime); 
 let circleTimeProgress = 0; 
 
 const addZero = (number) => {
@@ -44,13 +50,14 @@ const setTime = () => {
     updateTime(min, sec);
     updateCircleTime();
     clearTimer(); 
-}
+};
 
 const animateCircle = (duration) => {
     const parentNode = document.querySelector('.app-circle');
+    const animationClass = breakID ? 'app-circle--border-red' : 'app-circle--border-green';
 
     const circleAnimation = document.createElement('div');
-    circleAnimation.classList.add('app-circle--border');
+    circleAnimation.classList.add(animationClass);
     parentNode.prepend(circleAnimation); 
 
     setTimeout(() => {
@@ -58,14 +65,29 @@ const animateCircle = (duration) => {
     }, duration);
 };
 
+const switchToBreakTime = () => {
+    remainingSessionTime = breakTime;
+    step = +(SVG_STROKE_DASHOFFSET / breakTime);
+
+    circleBorder.style.stroke = BREAK_TIME_COLOR;
+    intervalID = setInterval(setTime, 10); 
+};
+
 const clearTimer = () => {
     if (remainingSessionTime <= 0) {
         clearInterval(intervalID);
         animateCircle(10000);
 
+        if (breakID) {  
+            setTimeout(switchToBreakTime, 10000);
+        }
+
         minutes.textContent = '00';
         seconds.textContent = '00';
+
+        breakID = !breakID;
     }
+    return;
 };
 
 const stopTimer = () => {
@@ -84,7 +106,9 @@ const switchBtnToPause= () => {
 
 resetBtn.addEventListener('click', () => {
     remainingSessionTime = totalTime;
+    step = breakID ? +(SVG_STROKE_DASHOFFSET / totalTime) : +(SVG_STROKE_DASHOFFSET / breakTime);
     circleTimeProgress = Math.abs(step);
+    circleBorder.style.stroke = SESSION_TIME_COLOR;
 
     updateCircleTime();
     updateTime();
